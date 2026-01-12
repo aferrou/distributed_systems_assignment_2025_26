c9e88d2494161ceb18e5d7b7f7938a6c7bc97c8e
Alexandra Ferrou <alexandra.ferrou@gmail.com>
2026-01-12 12:48:34 +0200

    Ignore local DB files

--- src/main/java/gr/hua/dit/fittrack/core/service/model/DeclineAppointmentRequest.java
+++
@@ -1,9 +0,0 @@
-package gr.hua.dit.fittrack.core.service.model;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record DeclineAppointmentRequest(
-        @NotNull Long id,
-        @NotBlank String declineReason
-) {}
