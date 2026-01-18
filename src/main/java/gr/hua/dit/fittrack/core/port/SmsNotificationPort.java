d5c0e7e96b1ffbcc76f3da3b6c4e8efb37795c90
Alexandra Ferrou <alexandra.ferrou@gmail.com>
2026-01-18 21:17:04 +0200

    Remove unused classes

--- src/main/java/gr/hua/dit/fittrack/core/port/SmsNotificationPort.java
+++
@@ -1,9 +0,0 @@
-package gr.hua.dit.fittrack.core.port;
-
-/**
- * Port to external service for managing SMS notifications.
- */
-public interface SmsNotificationPort {
-
-    boolean sendSms(final String e164, final String content);
-}
--- src/main/java/gr/hua/dit/fittrack/core/port/impl/dto/SendSmsRequest.java
+++
@@ -1,6 +0,0 @@
-package gr.hua.dit.fittrack.core.port.impl.dto;
-
-/**
- * SendSmsRequest DTO.
- */
-public record SendSmsRequest(String e164, String content) {}
